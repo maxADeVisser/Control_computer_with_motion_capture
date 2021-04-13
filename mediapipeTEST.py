1be6d8d6b573a6e0b45fe6e5212aceccb0c988c5
Computerliebe93 <70689092+Computerliebe93@users.noreply.github.com>
2021-04-13 13:05:42 +0200

    Update mediapipeTEST.py
    Drag and right click in dev

--- mediapipeTEST.py
+++ mediapipeTEST.py
@@ -2,16 +2,22 @@ import cv2
 import mediapipe as mp
 import pyautogui, sys
 import numpy as np
+import time #only used to make a break in between printing the coordinates of the fingers
+
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
+screen_size = pyautogui.size() #returns screen resolution as a tuple list: [x-cordinate, y-coordinate]
+window_size_x = screen_size[0]
+window_size_y = screen_size[1]
+
 # For webcam input:
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #initiates the webcam for use (0 = inbuild camera)
 with mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
+
   while cap.isOpened():
-    # pyautogui.FAILSAFE = False
     success, image = cap.read()
     if not success:
       print("Ignoring empty camera frame.")
@@ -33,32 +39,78 @@ with mp_hands.Hands(
       for hand_landmarks in results.multi_hand_landmarks:
         mp_drawing.draw_landmarks(
             image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            #alters whether the coordinates of the fingers are printed to the terminal
         """ print(
           f'Thumb tip coordinates: (',
           f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x}, '
           f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y})'
       )  """
-      pyautogui.moveTo(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * 1280, hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * 800)
+
+      #Makes the mouse move
+      pyautogui.moveTo(hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_CMC].x * 
+      window_size_x, hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_CMC].y * window_size_y)
       
+      #Euclidean distance index finger tip
       index_finger_posX = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x
       index_finger_posY = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
 
+      #Euclidean distance thumb tip
       thumb_posX = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x
-      thumb_posY = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
+      thumb_posY = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y
 
-      index_finger = np.array((index_finger_posX, index_finger_posY))
-      thumb = np.array((thumb_posX, thumb_posY))
-      
-      dist = np.linalg.norm(index_finger - thumb)
-      print(dist)
-      click = 0.001
-      if dist < click:
+      #Euclidean distance index finger mcp
+      index_finger_mcp_posX = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].x
+      index_finger_mcp_posY = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].y
+
+      #Euchlidean distiance pinky mcp
+      pinky_mcp_posX = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].x
+      pinky_mcp_posY = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].y
+
+      index_finger = np.array((hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x, hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y))
+      thumb = np.array((hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x, hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y))      
+      index_finger_mcp = np.array((hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].x, hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].y))
+      pinky_mcp = np.array((hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].x, hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].y))
+
+
+      # index_finger = np.array((index_finger_posX, index_finger_posY))
+      # thumb = np.array((thumb_posX, thumb_posY))      
+      # index_finger_mcp = np.array((index_finger_mcp_posX, index_finger_mcp_posY))
+      # index_finger = np.array((index_finger_posX, index_finger_posY))
+      # pinky_mcp = np.array((pinky_mcp_posX, pinky_mcp_posY))
+
+      # click functionalitet
+      #Click
+      left_click_dist = np.linalg.norm(thumb - index_finger_mcp)
+      # print(click_dist)
+      left_click = 0.04
+      if left_click_dist < left_click:
         pyautogui.click() 
-        print('Click')
+        print('Left click')
+
+
+      # Right click
+      right_click_dist = np.linalg.norm(thumb - pinky_mcp)
+      print(right_click_dist)
+      right_click = 0.04
+      if right_click_dist < right_click:
+        pyautogui.click(button = 'right')
+        print('Right click')
   
-    cv2.imshow('MediaPipe Hands', image)
+      # Mouse drag  
+      drag_dist = np.linalg.norm(index_finger - index_finger_mcp)
+      # print(drag_dist)
+      drag = 0.05
+      if drag_dist > drag:
+        pyautogui.mouseUp()
+      if drag_dist < drag:
+        pyautogui.mouseDown()
+        print('Drag')
+      
+
+    # cv2.imshow('MediaPipe Hands', image)
     
     if cv2.waitKey(5) & 0xFF == 27:
       break
     
 cap.release()
+
